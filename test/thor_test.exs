47afcf5f75090c672f9a84513313d09e145b7be4
Zack Kayser <zkayser@gmail.com>
2025-03-03 06:25:32 -0500

    Add first test case for adding cacheable assigns

--- test/thor_test.exs
+++ test/thor_test.exs
@@ -2,7 +2,19 @@ defmodule ThorTest do
   use ExUnit.Case
   doctest Thor
 
-  test "greets the world" do
-    assert Thor.hello() == :world
+  defmodule Subject do
+    use Thor
+
+    cacheable(:my_assign)
+    cacheable(:other_assign)
+  end
+
+  describe "use Thor" do
+    test "generates a __thor_assigns__/0 function on the using module" do
+      assert MapSet.equal?(
+               MapSet.new(Subject.__thor_assigns__()),
+               MapSet.new([:my_assign, :other_assign])
+             )
+    end
   end
 end
